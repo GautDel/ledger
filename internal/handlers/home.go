1d801012306c7d22ff6e42af093fc57346db9049
Gauthier Delalleau <gaut.del@proton.me>
2024-04-28 15:20:04 +0100

    Started on PDF generation

--- internal/handlers/home.go
+++ internal/handlers/home.go
@@ -4,11 +4,11 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"ledgerbolt.systems/internal/pdf"
 )
 
 func homeHandler(ctx *gin.Context) {
-
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": `Welcome to the home route of ledgerbolt API. Docs will be here in the future. Yours truly, Gauthier - The Dev`})
+    pdf.New()
+    ctx.JSON(http.StatusOK, gin.H{
+        "message": `Welcome to the home route of ledgerbolt API. Docs will be here in the future. Yours truly, Gauthier - The Dev`})
 }
-
--- internal/pdf/pdf.go
+++ internal/pdf/pdf.go
@@ -1,134 +1,111 @@
 package pdf
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strings"
+	"reflect"
 
 	"github.com/go-pdf/fpdf"
 )
 
-func strDelimit(str string, sepstr string, sepcount int) string {
-	pos := len(str) - sepcount
-	for pos > 0 {
-		str = str[:pos] + sepstr + str[pos:]
-		pos = pos - sepcount
-	}
-	return str
+type company struct {
+	name    string
+	address string
+	tel     string
+	email   string
+	compNum string
+}
+
+type page struct {
+	width  float64
+	height float64
+	margin float64
 }
 
 func New() {
 
+	// SETUP
 	pdf := fpdf.New("P", "mm", "A4", "") // 210mm x 297mm
+	page := page{
+		width:  210.00,
+		height: 297.00,
+		margin: 10.00,
+	}
+
 	pdf.AddPage()
 
-	// Create dynamic cell
-	//	text := "Name:"
-	//	textWidth := pdf.GetStringWidth(text)
-	//	log.Println(textWidth)
-	//	pdf.Cell(textWidth, 4, text)
-	//
-	//	// Create second dynamic cell
-	//	pdf.SetFont("Arial", "", 16)
-	//	text = "Gauthier"
-	//	textWidth = pdf.GetStringWidth(text)
-	//	log.Println(textWidth)
-	//	pdf.Cell(textWidth, 4, text)
+	// SET HEADER
+	pdf.SetFont("Arial", "B", 16)
+	header := "Identification de l'entrepreneur"
 
-	pdf.SetFont("Arial", "B", 12)
-	type countryType struct {
-		nameStr, capitalStr, areaStr, popStr string
+	c := company{
+		name:    "Aoife Anne McNamara, EI",
+		address: "Bâtiment A1, Résidence Eden Green, 935 Chemin du Golf, 30900 Nîmes, France",
+		tel:     "+33 7 89 34 23 41",
+		email:   "aoife.amcnamara@gmail.com",
+		compNum: "SIRET: 982 544 652 00018",
 	}
 
+	// Get the type of the struct
+	structType := reflect.TypeOf(c)
 
-	countryList := make([]countryType, 0, 8)
+	// Loop over the fields of the struct
 
-	header := []string{"Country", "Capital", "Area (sq km)", "Pop. (thousands)"}
+	companyTable := func() {
 
-	loadData := func(fileStr string) {
-		fl, err := os.Open(fileStr)
-		if err == nil {
-			scanner := bufio.NewScanner(fl)
-			var c countryType
-			for scanner.Scan() {
-				// Austria;Vienna;83859;8075
-				lineStr := scanner.Text()
-				list := strings.Split(lineStr, ";")
-				if len(list) == 4 {
-					c.nameStr = list[0]
-					c.capitalStr = list[1]
-					c.areaStr = list[2]
-					c.popStr = list[3]
-					countryList = append(countryList, c)
-				} else {
-					err = fmt.Errorf("error tokenizing %s", lineStr)
-				}
-			}
-			fl.Close()
-			if len(countryList) == 0 {
-				err = fmt.Errorf("error loading data from %s", fileStr)
-			}
-		}
-		if err != nil {
-			pdf.SetError(err)
-		}
-	}
+		left := page.margin
 
-	// Simple table
-	basicTable := func() {
-		// Page width - num cells * cell width / 2 -> 2 = center
-		left := (210.0 - 4*40) / 2
 		pdf.SetX(left)
-		for _, str := range header {
-			pdf.CellFormat(40, 7, str, "1", 0, "", false, 0, "")
-		}
+		tr := pdf.UnicodeTranslatorFromDescriptor("")
+		pdf.MultiCell(
+			(page.width / 3),
+			(12/2)+4,
+			tr(header),
+			"LTRB",
+			"M",
+			false,
+		)
 
-		pdf.Ln(-1)
+		next := page.margin + (page.width/3) + page.margin
 
-		for _, c := range countryList {
+		pdf.SetXY(next, 0 + page.margin)
+		tr = pdf.UnicodeTranslatorFromDescriptor("")
+		pdf.MultiCell(
+			(page.width / 3),
+			(12/2)+4,
+			tr(header),
+			"LTRB",
+			"M",
+			false,
+		)
+
+
+
+		for i := 0; i < structType.NumField(); i++ {
+			// Get the field name and value
+			fieldName := structType.Field(i).Name
+			fieldValue := reflect.ValueOf(c).Field(i).String()
+
+			// Print the field name and value
+			fmt.Printf("%s: %s\n", fieldName, fieldValue)
+
 			pdf.SetX(left)
-			pdf.CellFormat(40, 6, c.nameStr, "1", 0, "", false, 0, "")
-			pdf.CellFormat(40, 6, c.capitalStr, "1", 0, "", false, 0, "")
-			pdf.CellFormat(40, 6, c.areaStr, "1", 0, "", false, 0, "")
-			pdf.CellFormat(40, 6, c.popStr, "1", 0, "", false, 0, "")
-			pdf.Ln(-1)
+			pdf.SetFont("Helvetica", "", 12)
+			pdf.SetCellMargin(2.0)
+
+			tr := pdf.UnicodeTranslatorFromDescriptor("")
+			pdf.MultiCell(
+				(page.width / 3),
+				(12/2)+2,
+				tr(fieldValue),
+				"LTRB",
+				"M",
+				false,
+			)
 		}
+
 	}
 
-	// Better table
-	improvedTable := func() {
-		// Column widths
-		w := []float64{40.0, 35.0, 40.0, 45.0}
-		wSum := 0.0
-		for _, v := range w {
-			wSum += v
-		}
-		left := (210 - wSum) / 2
-		// 	Header
-		pdf.SetX(left)
-		for j, str := range header {
-			pdf.CellFormat(w[j], 7, str, "1", 0, "C", false, 0, "")
-		}
-		pdf.Ln(-1)
-		// Data
-		for _, c := range countryList {
-			pdf.SetX(left)
-			pdf.CellFormat(w[0], 6, c.nameStr, "LR", 0, "", false, 0, "")
-			pdf.CellFormat(w[1], 6, c.capitalStr, "LR", 0, "", false, 0, "")
-			pdf.CellFormat(w[2], 6, strDelimit(c.areaStr, ",", 3),
-				"LR", 0, "R", false, 0, "")
-			pdf.CellFormat(w[3], 6, strDelimit(c.popStr, ",", 3),
-				"LR", 0, "R", false, 0, "")
-			pdf.Ln(-1)
-		}
-		pdf.SetX(left)
-		pdf.CellFormat(wSum, 0, "", "T", 0, "", false, 0, "")
-	}
-
-	loadData("examplepdf.txt")
-	basicTable()
-	improvedTable()
+	companyTable()
 
 	// Create file
 	err := pdf.OutputFileAndClose("output.pdf")
